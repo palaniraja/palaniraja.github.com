880746c440bdf9efc640c626bde5f15ef75ea91d
palaniraja <palaniraja@live.com>
2018-08-17 12:51:33 +0530

    publish

--- publish.sh
+++ publish.sh
@@ -1,11 +1,18 @@
 #!/usr/bin
 
-echo "Going to remove previously generated files"
+echo "[ 1/3 ] Going to remove previously generated files"
 # to delete files that are generated (.git and .ignore as they are hidden)
 rm -rf `ls | grep -v "CNAME\|hugo\|README.md\|publish.sh"`
 
 # switch to hugo directory and run hugo to build genereate content in hugo/docs and move generated files back to ./
-echo "To build static contents and copy"
+echo "[ 2/3 ] To build static contents and copy"
 cd hugo && hugo && mv docs/* ../ && cd ..
+echo "COMPLETE"
+
+# git commit changes as "publish"
+echo "[ 3/3 ] Commit changes and push to github"
+git add .
+git commit -m "publish"
+git push origin --all
 
 echo "COMPLETE"
